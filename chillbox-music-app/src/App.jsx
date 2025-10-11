f643ccac16d31395e4594d51aa10a3be29c67ca8
Shaun Manda <shaunwmanda@gmail.com>
2025-10-11 15:07:47 +0300

    add AuthProvider , SignUpPage and Router imports to main.jsx

--- chillbox-music-app/src/App.jsx
+++ chillbox-music-app/src/App.jsx
@@ -2,11 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import SignUpPage from './components/SignUpPage.jsx';
+import { Switch } from 'react-router-dom';
+import authProvider from './components/AuthProvider.jsx';
 
 function App() {
   return(
-    <h1 className='text-2xl text-red-500'>whatsApp there</h1>
+    <Router>
+      <h1 className='text-3xl text-red-500 flex-start bg-gray-100'>Home</h1>
+      <Switch>
+        <Router path="/signup" component={SignUpPage} />
+      </Switch>
+    </Router>
   )
 }
 
-export default App
+export default App;
