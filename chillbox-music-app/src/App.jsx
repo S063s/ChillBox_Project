cbbc1d29d354a9e82068f2e539a9b5cff4c9ace4
Shaun Manda <shaunwmanda@gmail.com>
2025-10-12 12:35:27 +0300

    add musicplayer page route to app.jsx and deleted files files fro, components

--- chillbox-music-app/src/App.jsx
+++ chillbox-music-app/src/App.jsx
@@ -2,20 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter as Router } from 'react-router-dom';
-import SignUpPage from './components/SignUpPage.jsx';
-import { Switch } from 'react-router-dom';
-import authProvider from './components/AuthProvider.jsx';
+import{ createBrowserRouter , RouterProvider } from 'react-router';
+import SignUpPage from './Pages/SignUpPage.jsx';
+import SearchPage from './Pages/SearchPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/signup",
+    element: <SignUpPage />,
+  },
+  {
+    path: "/search",
+    element: <SearchPage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  }
+]);
 
 function App() {
   return(
-    <Router>
-      <h1 className='text-3xl text-red-500 flex-start bg-gray-100'>Home</h1>
-      <Switch>
-        <Router path="/signup" component={SignUpPage} />
-      </Switch>
-    </Router>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
