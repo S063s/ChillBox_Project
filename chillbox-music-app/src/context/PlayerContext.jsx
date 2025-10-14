b7715df2fcf70692f3346a82d55ad2b11920411c
Shaun Manda <shaunwmanda@gmail.com>
2025-10-14 19:14:16 +0300

    add PlayerContext to handle global state for music playback

---
+++ chillbox-music-app/src/context/PlayerContext.jsx
@@ -0,0 +1,63 @@
+import { createContext , useContext , useState , useRef , useEffect } from "react";
+import axios from "axios";
+
+const PlayerContext = createContext();
+const API_URL = "https://api.deezer.com/track/";
+
+export function usePlayer() {
+    const audioRef = useRef(new Audio());
+    const [currentTrack, setCurrentTrack] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [duration, setDuration] = useState(0);
+    const [volume, setVolume] = useState(1);
+    const [isMuted, setIsMuted] = useState(false);
+
+   const loadAndPlayTrack = async (trackId) => {
+       try {
+           const response = await axios.get(`${API_URL}${trackId}`);
+           const track = response.data;
+           setCurrentTrack({
+                id: track.id,
+                title: track.title,
+                artist: track.artist.name,
+                album: track.album.title,
+                duration: track.duration,
+                preview: track.preview,
+           });
+           audioRef.current.src = track.preview;
+           audioRef.current.play();
+           setIsPlaying(true);
+       } catch (error) {
+           console.error("Error fetching track:", error);
+       }
+   };
+
+   return (
+       <PlayerContext.Provider
+           value={{
+               currentTrack,
+               isPlaying,
+               loadAndPlayTrack,
+               audioRef,
+               setIsPlaying,
+               currentTime,
+               setCurrentTime,
+               duration,
+               setDuration,
+               volume,
+               setVolume,
+               isMuted,
+               setIsMuted,
+           }}
+       >
+           {children}
+       </PlayerContext.Provider>
+   );
+}
+
+export const usePlayerContext = () => {
+   return useContext(PlayerContext);
+};
+
+export default PlayerContext;
